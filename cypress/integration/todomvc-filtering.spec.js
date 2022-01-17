69fce690c6ff3042bda93526fcede173d4f0b6a8
NickolaiL <nixon2234392@gmail.com>
2022-01-17 14:14:23 +0300

    Removed example tests and added tests for filtering

--- cypress/integration/todomvc-filtering.spec.js
+++ cypress/integration/todomvc-filtering.spec.js
@@ -3,7 +3,23 @@
 describe('Todo MVC filtering', () => {
     beforeEach( () => {
         cy.visit('http://todomvc-app-for-testing.surge.sh')
-        cy.get('.new-todo', {timeout: 5000}).type('New Todo item{enter}')
+        cy.get('.new-todo').type('First Todo item{enter}')
+        cy.get('.new-todo').type('Second Todo item{enter}')
+        cy.get('.todo-list li:nth-child(2) .toggle').click()
+        cy.get('.new-todo').type('Third Todo item{enter}')
+    })
+    it('Showing all elements ', () => {
+        cy.get('.filters').contains('All').click()
+        cy.get('.todo-list li').should('have.length', 3)
     })
 
+    it('Showing completed elements ', () => {
+        cy.get('.filters').contains('Active').click()
+        cy.get('.todo-list li').should('have.length', 2)
+    })
+
+    it('Showing active elements ', () => {
+        cy.get('.filters').contains('Completed').click()
+        cy.get('.todo-list li').should('have.length', 1)
+    })
 })
