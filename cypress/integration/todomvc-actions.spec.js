d798a91028d6c0b724e51be560e680f7d58f4566
NickolaiL <nixon2234392@gmail.com>
2022-01-18 09:01:37 +0300

    Added visual validation with applitools eyes

--- cypress/integration/todomvc-actions.spec.js
+++ cypress/integration/todomvc-actions.spec.js
@@ -14,20 +14,34 @@ describe('Todo MVC actions', () => {
     beforeEach( () => {
         navigate()
         addTodo("New Todo item")
+        cy.eyesOpen({
+            appName: "Todo test", 
+            batchName: "Todo test batch", 
+            browser: [
+                {name: 'chrome', width: 1024, height: 768},
+                {name: 'firefox', width: 1024, height: 768},
+                {deviceName: 'iPhone X', screenOrientation: 'portrait'},
+            ]
+        })
     })
+    afterEach(() => cy.eyesClose())
+
     it('Should add a new todo item', () => {
         validateTodoText(0, 'New Todo item')
         validateToggleState(0, false)
+        cy.eyesCheckWindow('1 Todo added')
     })
 
     it('Should mark a todo as completed', () => {
         toggleTodo(0)
         validateToggleState(0, true)
+        cy.eyesCheckWindow('Todo is completed')
     })
     
     it('Should clean completed todos', () => {
         toggleTodo(0)
         clearCompleted()
         validateNumberOfTodosShown(0)
+        cy.eyesCheckWindow('Todos are cleared')
     })
 })
